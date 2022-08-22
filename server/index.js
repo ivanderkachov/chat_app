b93d6642b0f1622d3b5c4be8fcd4c252bf189fbe
Деркачев Иван <derkachov_ivan@host-192-168-0-103.sohonet.ua>
2022-08-22 12:08:39 +0300

    another commit

--- server/index.js
+++ server/index.js
@@ -23,11 +23,12 @@ mongooseService.connect(process.env.MONGODB_URI)
 app.use(cors());
 app.use(router)
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../build")))
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build", "index.html"))
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
   })
 } else {
   app.get("/", (req, res) => {
