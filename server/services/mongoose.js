2a70b6086da33c761bd0b7d15c8bc9769717c878
Деркачев Иван <derkachov_ivan@host-192-168-0-103.sohonet.ua>
2022-08-22 08:08:18 +0300

    10 commit

--- server/services/mongoose.js
+++ server/services/mongoose.js
@@ -9,7 +9,7 @@ mongoose.connection.on('error', (err) => {
   process.exit(1)
 })
 
-exports.connect = async (mongoURL = '127.0.0.1:27017/db') => {
+exports.connect = async (mongoURL = 'mongodb+srv://vercel-admin-user-63029d53e7cfd9142daa7a36:5fKWpm9WDEQ3kiUa@cluster0.uwzfx.mongodb.net/admin') => {
   mongoose.connect(mongoURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
