c0c811b1d934dc5a9525c45d706e6e7f49bc1c70
Деркачев Иван <derkachov_ivan@host-192-168-0-103.sohonet.ua>
2022-08-22 15:30:33 +0300

    commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 
 const express = require('express')
-const http = require('http')
+// const http = require('http')
 const cors = require('cors')
 const axios = require('axios')
 const path = require('path')
@@ -23,27 +23,27 @@ mongooseService.connect(process.env.MONGODB_URI)
 app.use(cors());
 app.use(router)
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../build")))
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build/index.html"), function (err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    });
-  })
-} else {
-  app.get("/", (req, res) => {
-    res.send("Api running")
-  })
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../build")))
+//   app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../build/index.html"), function (err) {
+//       if (err) {
+//         res.status(500).send(err)
+//       }
+//     });
+//   })
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("Api running")
+//   })
+// }
 
-console.log(process.env.NODE_ENV)
-// app.get("/", (req, res) => {
-//   res.sendFile("index.html", { root: path.join(__dirname, "../public") });
-// });
+// console.log(process.env.NODE_ENV)
+app.get("/", (req, res) => {
+  res.send("Server");
+});
 
 // router.get("/", (req, res) => {
 //   res.json("Server up and running");
@@ -151,4 +151,4 @@ app.listen(port, () => {
   console.log(`Server has started on port ${port}`)
 })
 
-module.exports = app;
+// module.exports = app;
