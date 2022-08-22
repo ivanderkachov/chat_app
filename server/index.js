462e4aa290d704a8a358690418749e2202b9ceb7
Деркачев Иван <derkachov_ivan@host-192-168-0-103.sohonet.ua>
2022-08-22 16:37:06 +0300

    commut

--- server/index.js
+++ server/index.js
@@ -28,9 +28,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")))
+  app.use(express.static(path.join(__dirname, "../build")))
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'), function (err) {
+    res.sendFile(path.join(__dirname, '../build', '../build/index.html'), function (err) {
       if (err) {
         res.status(500).send(err)
       }
